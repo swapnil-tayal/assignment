ddbe4c605a2a8c4411147fa6d38415905099f2c6
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-04-15 10:34:56 +0530

    Add files via upload

---
+++ react/src/App.js
@@ -0,0 +1,83 @@
+import react, { useEffect, useState } from "react";
+
+const apiKey = "633be71e9ba941b4aba43319241004";
+function App() {
+  const [city, setCity] = useState("");
+  const [cityArr, setCityarr] = useState([]);
+
+  const handleButtonClick = async () => {
+    const res = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`
+    );
+    const response = await res.json();
+    if (!response) {
+      alert("invalid city name");
+      return;
+    }
+    if (!response.current) {
+      alert("invalid city name");
+      return;
+    }
+    const temp = response.current.temp_c;
+
+    if (!temp) {
+      alert("invalid city name");
+    } else {
+      setCityarr((prevState) => ({
+        [city]: temp,
+        ...prevState,
+      }));
+      localStorage.setItem(city, temp);
+    }
+  };
+
+  const handleDelete = (city) => {
+    console.log(city);
+    localStorage.removeItem(city);
+    const currentState = {...cityArr};
+    delete currentState[city];
+    setCityarr(currentState);
+  }
+
+  const handleUpdate = async(city) => {
+    const res = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`
+    );
+    const response = await res.json();
+    const temp = response.current.temp_c;
+    localStorage.setItem(city, temp);
+    const currentState = cityArr;
+    currentState[city] = temp;
+    console.log(city, temp);
+  }
+
+  useEffect(() => {
+    const keys = Object.keys(localStorage);
+    const localStorageObject = {};
+    keys.forEach((key) => {
+      const value = localStorage.getItem(key);
+      localStorageObject[key] = value;
+    });
+    setCityarr(localStorageObject);
+  }, []);
+
+  return (
+    <>
+      <label>Zip Code</label>
+      <input value={city} onChange={(e) => setCity(e.target.value)} />
+      <br />
+      <button onClick={handleButtonClick}> CLick me </button>
+      <ul>
+        {Object.entries(cityArr).map(([city, value], index) => {
+          return [
+            <li key={index}> {city} : {value} </li>,
+            <button key={index+1} onClick={() => handleDelete(city)} >delete</button>,
+            <button key={index+2} onClick={() => handleUpdate(city)} >update</button>
+          ];
+        })}
+      </ul>
+    </>
+  );
+}
+
+export default App;
